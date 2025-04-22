6b92021fdcfbd01d27818630161812dc89ac8ef4
Jewels <jewels286@proton.me>
2025-04-22 15:44:22 -0400

    commit before i try a new sysem

--- Opal/IModule.cs
+++ Opal/IModule.cs
@@ -9,6 +9,7 @@ namespace Opal
 	public interface IModule
 	{
 		public string ID { get; }
+		public string IsInput { get; }
 		public void Initialize(Context ctx);
 		public void Receive(Signal sig);
 		public List<Signal> Step(Context ctx);
@@ -16,6 +17,9 @@ namespace Opal
 	public interface IAsyncModule
 	{
 		public string ID { get; }
+		public string IsInput { get; }
+		public bool IsFinished { get; }
+
 		public Task InitializeAsync(Context ctx);
 		public Task ReceiveAsync(Signal sig);
 		public Task StepAsync(Context ctx);
---
+++ Opal/Modules/StringInputModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Opal.Modules
+{
+	public class StringInputModule : IModule
+	{
+		public string ID => "string-input";
+		public bool IsInput => true; 
+		public Stream Input { get; set; }
+
+		public 
+
+		public void Initialize(Context ctx) { }
+		public void Receive(Signal sig) { }
+		public void Step(Context ctx)
+		{
+
+		}
+	}
+}
