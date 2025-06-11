52d6b5f28c3f06df722eb27204d8c26d633ae5d5
Jewels <jewels286@proton.me>
2025-06-11 15:30:13 -0400

    Add RNN layer implementation with activation function support and state management

--- Opal/Utilities/ANNs/ILayer.cs
+++ Opal/Utilities/ANNs/ILayer.cs
@@ -5,4 +5,7 @@ public interface ILayer
     public double[] Forward(double[] input);
     public double[] Backward(double[] gradOutput, double learningRate);
     public void Reset();
+    
+    public delegate double[] ActivationFunction(double[] input);
+    public delegate double[] ActivationFunctionDerivative(double[] input); 
 }
---
+++ Opal/Utilities/ANNs/Recurrent/IRecurrentLayer.cs
@@ -0,0 +1,8 @@
+﻿namespace Opal.Utilities.ANNs.Recurrent;
+
+public interface IRecurrentLayer : ILayer
+{
+    public void ResetState();
+    public double[][] Forward(double[] input, double[][] previousState);
+    public double[][] Backward(double[][] gradOutput, double learningRate);
+}
