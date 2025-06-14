bfc98e605d24b3184495975b33f3b18998cb4087
Jewels <jewels286@proton.me>
2025-06-14 16:37:51 -0400

    Enhance LstmLayer with activation functions, state management, and forward pass implementation; add utility functions in MathFunctions for matrix and vector operations

--- Opal/Utilities/ANNs/Recurrent/LstmLayer.cs
+++ Opal/Utilities/ANNs/Recurrent/LstmLayer.cs
@@ -2,7 +2,102 @@
 
 using static MathFunctions;
 
-public class LstmLayer : IRecurrentLayer
+public class LstmLayer
 {
+    public ILayer.ActivationFunction SigmoidFunction { get; set; } = Sigmoid;
+    public ILayer.ActivationFunction TanhFunction { get; set; } = Tanh;
+
+    public int InputSize { get; private set; }
+    public int HiddenSize { get; private set; }
+    public int BatchSize { get; private set; }
     
+    public List<double[]> CellStates { get; private set; } = [];
+    public List<double[]> HiddenStates { get; private set; } = [];
+    public List<double[]> InputStates { get; private set; } = [];
+    
+    public double[,] ForgetGateWeight { get; set; }
+    public double[,] InputGateWeight { get; set; }
+    public double[,] CellStateWeight { get; set; }
+    public double[,] OutputGateWeight { get; set; }
+    
+    public double[] ForgetGateBias { get; set; }
+    public double[] InputGateBias { get; set; }
+    public double[] CellStateBias { get; set; } 
+    public double[] OutputGateBias { get; set; }
+
+    private List<double[]> _forgetGates = [];
+    private List<double[]> _inputGates = [];
+    private List<double[]> _cellCandidates = [];
+    private List<double[]> _outputGates = [];
+    private List<double[]> _concatInputs = [];
+    private List<List<double[]>> _cellStateTimeline = [];
+    private List<List<double[]>> _hiddenStateTimeline = [];
+
+    public LstmLayer(int inputSize, int hiddenSize, int batchSize)
+    {
+        InputSize = inputSize;
+        HiddenSize = hiddenSize;
+        BatchSize = batchSize;
+        
+        ForgetGateWeight = RandomMatrix(inputSize + hiddenSize, hiddenSize);
+        InputGateWeight = RandomMatrix(inputSize + hiddenSize, hiddenSize);
+        CellStateWeight = RandomMatrix(inputSize + hiddenSize, hiddenSize);
+        OutputGateWeight = RandomMatrix(inputSize + hiddenSize, hiddenSize);
+
+        ForgetGateBias = new double[HiddenSize];
+        InputGateBias = new double[HiddenSize];
+        CellStateBias = new double[HiddenSize];
+        OutputGateBias = new double[HiddenSize];
+    }
+
+    public double[,,] Forward(double[,,] inputSequence, bool reset = true)
+    {
+        if (reset) ResetState();
+        int batch = inputSequence.GetLength(0);
+        int time = inputSequence.GetLength(1);
+        int inputSize = inputSequence.GetLength(2);
+
+        double[,,] output = new double[batch, time, HiddenSize];
+
+        for (int i = 0; i < batch; i++)
+        {
+            for (int j = 0; j < time; j++)
+            {
+                double[] input = GetInputFromSample(GetBatchSample(inputSequence, i), j);
+                double[] concat = input.Concat(HiddenStates[i]).ToArray();
+
+                double[] forgetGate = Apply(SigmoidFunction, Add(Multiply(ForgetGateWeight, concat), ForgetGateBias));
+                double[] inputGate = Apply(SigmoidFunction, Add(Multiply(InputGateWeight, concat), InputGateBias));
+                double[] cellCandidate = Apply(TanhFunction, Add(Multiply(CellStateWeight, concat), CellStateBias));
+
+                double[] newCellState = Add(
+                    Multiply(forgetGate, CellStates[i]),
+                    Multiply(inputGate, cellCandidate));
+                double[] outputGate = Apply(SigmoidFunction, Add(Multiply(OutputGateWeight, concat), OutputGateBias));
+                double[] hiddenState = Multiply(outputGate, Apply(TanhFunction, newCellState));
+
+                CellStates[i] = newCellState;
+                HiddenStates[i] = hiddenState;
+                InputStates[i] = input;
+
+                for (int h = 0; h < HiddenSize; h++)
+                    output[i, j, h] = hiddenState[h];
+            }
+        }
+        return output;
+    }
+
+    public void ResetState()
+    {
+        CellStates.Clear();
+        HiddenStates.Clear();
+        InputStates.Clear();
+        
+        for (int i = 0; i < BatchSize; i++)
+        {
+            CellStates.Add(new double[HiddenSize]);
+            HiddenStates.Add(new double[HiddenSize]);
+            InputStates.Add(new double[InputSize]);
+        }
+    }
 }
--- Opal/Utilities/MathFunctions.cs
+++ Opal/Utilities/MathFunctions.cs
@@ -25,11 +25,20 @@ public static class MathFunctions
     {
         return InitializeMatrix(rows, cols, (i, j) => Random.Shared.NextDouble() * 2 - 1);
     }
+    public static double[,] ZeroMatrix(int rows, int cols)
+    {
+        return InitializeMatrix(rows, cols, (i, j) => 0.0);
+    }
     
     public static double Sigmoid(double x) => 1.0 / (1.0 + Math.Exp(-x));
     public static double[] Sigmoid(double[] x) => x.Select(Sigmoid).ToArray();
     public static double Tanh(double x) => Math.Tanh(x);
     public static double[] Tanh(double[] x) => x.Select(Tanh).ToArray();
+    
+    public static double SigmoidDerivative(double x) => Sigmoid(x) * (1 - Sigmoid(x));
+    public static double[] SigmoidDerivative(double[] x) => x.Select(SigmoidDerivative).ToArray();
+    public static double TanhDerivative(double x) => 1.0 - Math.Pow(Tanh(x), 2);
+    public static double[] TanhDerivative(double[] x) => x.Select(TanhDerivative).ToArray();
 
     public static double[] Apply(ILayer.ActivationFunction fn, double[] x)
     {
@@ -57,6 +66,21 @@ public static class MathFunctions
     {
         return a.Select(x => x * scalar).ToArray();
     }
+
+    public static double[] Multiply(double[,] matrix, double[] vector)
+    {
+        if (matrix.GetLength(1) != vector.Length) throw new ArgumentException("Matrix columns must match vector length.");
+        double[] result = new double[matrix.GetLength(0)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            result[i] = 0;
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[i] += matrix[i, j] * vector[j];
+            }
+        }
+        return result;
+    }
     public static double[] Divide(double[] a, double scalar)
     {
         if (scalar == 0) throw new DivideByZeroException("Cannot divide by zero.");
@@ -66,5 +90,24 @@ public static class MathFunctions
     {
         if (a.Length != b.Length) throw new ArgumentException("Vectors must be of the same length.");
         return new double[] { a.Zip(b, (x, y) => x * y).Sum() };
+    }
+    
+    public static double[,] GetBatchSample(double[,,] inputSequence, int batchIdx)
+    {
+        int time = inputSequence.GetLength(1);
+        int inputSize = inputSequence.GetLength(2);
+        double[,] sample = new double[time, inputSize];
+        for (int t = 0; t < time; t++)
+        for (int i = 0; i < inputSize; i++)
+            sample[t, i] = inputSequence[batchIdx, t, i];
+        return sample;
+    }
+    public static double[] GetInputFromSample(double[,] sample, int timeStep)
+    {
+        int inputSize = sample.GetLength(1);
+        double[] input = new double[inputSize];
+        for (int i = 0; i < inputSize; i++)
+            input[i] = sample[timeStep, i];
+        return input;
     }
 }
