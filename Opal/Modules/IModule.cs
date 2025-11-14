11cc83ca609ac24119b740999c0418011ead9bc8
Jewels <jewels286@proton.me>
2025-11-14 11:54:28 -0500

    fix: moved IModule.cs

--- Opal/Modules/IModule.cs
+++ Opal/Modules/IModule.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Opal.Utilities;
+﻿using Opal.Utilities;
 
-namespace Opal
+namespace Opal.Modules
 {
 	public interface IModule
 	{
