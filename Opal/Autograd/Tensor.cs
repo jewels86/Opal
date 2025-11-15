abd06f649aa8487b6868b06990f1f67ba6c7bfb5
Jewels <jewels286@proton.me>
2025-11-15 13:56:05 -0500

    feat: new tensor implementation

--- Opal/Autograd/Tensor.cs
+++ Opal/Autograd/Tensor.cs
@@ -1,4 +1,53 @@
-﻿namespace Opal.Autograd;
+﻿using ILGPU;
+using ILGPU.Runtime;
+
+namespace Opal.Autograd;
+
+using ScalarTensor = Tensor<ITensorStorage<double>>;
+using VectorTensor = Tensor<ITensorStorage<double[]>>;
+using MatrixTensor = Tensor<ITensorStorage<double[,]>>;
+
+public interface ITensorStorage<T> where T : notnull
+{
+    public T ToHost();
+    public void CopyFrom(T source);
+    public int[] Shape { get; }
+    public int TotalElements { get; }
+}
+
+public class CpuStorage<T> : ITensorStorage<T> where T : notnull
+{
+    public required T Data { get; set; }
+    public required int[] Shape { get; set; }
+    public required int TotalElements { get; set; }
+    
+    public T ToHost() => Data;
+    public void CopyFrom(T source) => Data = source;
+}
+
+public class GpuVectorStorage : ITensorStorage<double[]>
+{
+    public MemoryBuffer1D<double, Stride1D.Dense> GpuData { get; set; }
+    public int[] Shape => [(int)GpuData.Length];
+    public int TotalElements => (int)GpuData.Length;
+    
+    public GpuVectorStorage(MemoryBuffer1D<double, Stride1D.Dense> gpuData) => GpuData = gpuData;
+
+    public double[] ToHost() => GpuData.GetAsArray1D();
+    public void CopyFrom(double[] data) => GpuData.CopyFromCPU(data);
+}
+
+public class GpuMatrixStorage : ITensorStorage<double[,]>
+{
+    public MemoryBuffer2D<double, Stride2D.DenseX> GpuData { get; set; }
+    public int[] Shape => [(int)GpuData.Extent.X, (int)GpuData.Extent.Y];
+    public int TotalElements => (int)GpuData.Length;
+    
+    public GpuMatrixStorage(MemoryBuffer2D<double, Stride2D.DenseX> gpuData) => GpuData = gpuData;
+    
+    public double[,] ToHost() => GpuData.GetAsArray2D();
+    public void CopyFrom(double[,] data) => GpuData.CopyFromCPU(data);
+}
 
 public class Tensor<T> where T : notnull
 {
